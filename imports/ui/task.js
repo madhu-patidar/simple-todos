b2b5be1d3e870cf4896d49bbaff6bed7c694270a
madhu-patidar <madhusudan.patidar@neosofttech.com>
2017-04-07 18:55:50 +0530

    simple todos app

---
+++ imports/ui/task.js
@@ -0,0 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+
+import { Template } from 'meteor/templating';
+ 
+ 
+import './task.html';
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+ 
+Template.task.events({
+  'click .toggle-checked'() {
+     Meteor.call('tasks.setChecked', this._id, !this.checked);
+  },
+  'click .delete'() {
+      Meteor.call('tasks.remove', this._id);
+  },
+});
